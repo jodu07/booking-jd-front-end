0c4fffcb49feea9c36c426038b9fd1cf9efd9c92
jodu07 <jhonnat.dussan@gmail.com>
2025-06-11 11:55:56 +0200

    BOOKJD-1 Tov nav component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { NavComponent } from './navigation/navbar/nav/nav.component';
+/**
+ * Home component.
+ */
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/navigation/navbar/nav/nav.component.ts
+++ src/app/navigation/navbar/nav/nav.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { TopNavComponent } from './top-nav/top-nav.component';
+/**
+ * Nav component.
+ */
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TopNavComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
