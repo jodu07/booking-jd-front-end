95d4fa1ce19b7cb67fabb076955aea9fb49b8c84
jodu07 <jhonnat.dussan@gmail.com>
2025-06-13 23:14:02 +0200

    profile-user component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavComponent } from './navigation/navbar/nav/nav.component';
 import { FooterComponent } from './navigation/navbar/nav/footer/footer.component';
+import { ProfileUserComponent } from './user/profile-user/profile-user.component';
 /**
  * Home component.
  */
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NavComponent, FooterComponent],
+  imports: [CommonModule, NavComponent, FooterComponent, ProfileUserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
