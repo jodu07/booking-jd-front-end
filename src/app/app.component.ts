9c842b6d62db22f493740204c94af6ebca19225e
jodu07 <jhonnat.dussan@gmail.com>
2025-06-17 22:04:58 +0200

    Progress for rendering list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { NavComponent } from './navigation/navbar/nav/nav.component';
 import { FooterComponent } from './navigation/navbar/nav/footer/footer.component';
 import { CustomerProfileComponent } from './customer/customer-profile/customer-profile.component';
+import { ProfileUserComponent } from './user/profile-user/profile-user.component';
 /**
  * Home component.
  */
@@ -14,10 +15,17 @@ import { CustomerProfileComponent } from './customer/customer-profile/customer-p
     NavComponent,
     FooterComponent,
     CustomerProfileComponent,
+    ProfileUserComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'booking-jd-front-end';
+  selectedMenu: string | null = null;
+
+  onMenuSelected(menu: string) {
+    this.selectedMenu = menu;
+    console.log(this.selectedMenu);
+  }
 }
