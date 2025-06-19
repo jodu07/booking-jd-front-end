6c2be0a642d5599a1ea38520a3d9cdfbaee7aeb8
jodu07 <jhonnat.dussan@gmail.com>
2025-06-19 13:52:07 +0200

    Reservations and booking card settings

--- src/app/reservations/reservations/booking/booking.component.ts
+++ src/app/reservations/reservations/booking/booking.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-booking',
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   standalone: true,
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.scss',
