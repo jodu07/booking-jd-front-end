0721cc8c7970caf84e7b9436b4c685c496aa3fc7
jodu07 <jhonnat.dussan@gmail.com>
2025-06-19 13:28:21 +0200

    BOOKJD-9 - Booking component added

---
+++ src/app/reservations/reservations/booking/booking.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './booking.component.html',
+  styleUrl: './booking.component.scss',
+})
+export class BookingComponent {}
