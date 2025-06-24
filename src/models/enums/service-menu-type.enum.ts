9a68f5943bf604d9c9abbbfac69ed97862f5bde6
jodu07 <jhonnat.dussan@gmail.com>
2025-06-24 22:23:20 +0200

    ServiceMenuType added in enums models

---
+++ src/models/enums/service-menu-type.enum.ts
@@ -0,0 +1,29 @@
+/**
+ * Represents all the type of service menu.
+ */
+export enum ServiceMenuType {
+  /** Boooking type. */
+  Booking = 'Booking',
+  /** Gift Cards type. */
+  GiftCards = 'Gift Cards',
+  /** Family & Friends type. */
+  FamilyAndFriends = 'Family & Friends',
+  /** Favorites type. */
+  Favorites = 'Favorites',
+  /** Account & Settings type. */
+  AccountAndSettings = 'Account & Settings',
+  /** Reviews type. */
+  Reviews = 'Reviews',
+  /** Payments type. */
+  Payments = 'Payments',
+  /** Custom Forms type. */
+  CustomForms = 'Custom Forms',
+  /** Follow us on Facebook type. */
+  FollowUsOnFacebook = 'Follow us on Facebook',
+  /** Terms of Use type. */
+  TermsOfUse = 'Terms of Use',
+  /** Privacy Policy type. */
+  PrivacyPolicy = 'Privacy Policy',
+  /** Logout type. */
+  Logout = 'Logout',
+}
