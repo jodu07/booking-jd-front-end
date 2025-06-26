6627ef87586701efe544851fff8a2fafbc9975ea
jodu07 <jhonnat.dussan@gmail.com>
2025-06-26 19:45:32 +0200

    comments removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,6 @@ import { AdminComponent } from './admin/admin/admin.component';
 import { ProfileUserComponent } from './user/profile-user/profile-user.component';
 
 export const routes: Routes = [
-  /* {
-    path: 'admin',
-    loadComponent: () =>
-      import('./admin/admin/admin.component').then((m) => m.AdminComponent),
-  },*/
   { path: '', component: ProfileUserComponent },
   { path: 'admin', component: AdminComponent },
 ];
