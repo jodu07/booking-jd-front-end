d7bb58b29bd0dea105e2497812133416943722a7
jodu07 <jhonnat.dussan@gmail.com>
2025-06-29 17:34:56 +0200

    sign in component settings

--- src/app/entry/sign-in/sign-in.component.ts
+++ src/app/entry/sign-in/sign-in.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatFormFieldModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss',
 })
