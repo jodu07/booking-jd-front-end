8fcbb399c2f68a68397f3782c46d862eeda3995e
jodu07 <jhonnat.dussan@gmail.com>
2025-06-29 17:18:28 +0200

    sign-in component added

---
+++ src/app/entry/sign-in/sign-in.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.scss',
+})
+export class SignInComponent {}
