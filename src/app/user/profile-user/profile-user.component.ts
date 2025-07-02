b62238e5ef0f3ecbcf30527b5eed886fa9a5aa25
jodu07 <jhonnat.dussan@gmail.com>
2025-07-02 23:45:26 +0200

    get Customer for profile user

--- src/app/user/profile-user/profile-user.component.ts
+++ src/app/user/profile-user/profile-user.component.ts
@@ -4,6 +4,7 @@ import { AdminComponent } from 'src/app/admin/admin/admin.component';
 import { AuthService } from 'src/app/core/auth.service';
 import { CustomerProfileComponent } from 'src/app/customer/customer-profile/customer-profile.component';
 import { ReservationsComponent } from 'src/app/reservations/reservations/reservations.component';
+import { Customer } from 'src/models/customer';
 
 @Component({
   selector: 'app-profile-user',
@@ -18,9 +19,16 @@ import { ReservationsComponent } from 'src/app/reservations/reservations/reserva
   styleUrl: './profile-user.component.scss',
 })
 export class ProfileUserComponent {
+  loggedCustomer!: Customer;
   selectedMenu = '';
 
   constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.getCustomerObservable().subscribe((customer) => {
+      if (customer) this.loggedCustomer = customer;
+    });
+  }
 
   onMenuSelected(menu: string) {
     this.selectedMenu = menu;
