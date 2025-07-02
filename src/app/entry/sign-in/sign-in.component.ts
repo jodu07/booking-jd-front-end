107b9ab79261e37a4966ec535b374618ca3610bd
jodu07 <jhonnat.dussan@gmail.com>
2025-07-02 22:53:00 +0200

    SignIn component settings

--- src/app/entry/sign-in/sign-in.component.ts
+++ src/app/entry/sign-in/sign-in.component.ts
@@ -6,6 +6,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
@@ -20,6 +21,7 @@ import { MatInput } from '@angular/material/input';
     MatLabel,
     ReactiveFormsModule,
     MatInput,
+    MatButtonModule,
   ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss',
@@ -34,10 +36,5 @@ export class SignInComponent {
       username: ['', Validators.required],
       password: ['', Validators.required],
     });
-  }
-
-  /** Send form. */
-  submitForm(): void {
-    console.log('hola');
   }
 }
