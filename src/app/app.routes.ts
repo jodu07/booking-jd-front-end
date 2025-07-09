920e245cdd85cb63a6b48b8c45fefe48ff8be063
jodu07 <jhonnat.dussan@gmail.com>
2025-07-09 23:59:44 +0200

    admin guard added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,16 @@ import { HomeComponent } from './home/home/home.component';
 import { ProfileUserComponent } from './user/profile-user/profile-user.component';
 import { SignInComponent } from './entry/sign-in/sign-in.component';
 import { AccountCreateComponent } from './entry/account-create/account-create.component';
+import { authGuard } from './core/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'admin', component: AdminComponent },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./admin/admin/admin.component').then((m) => m.AdminComponent),
+    canActivate: [authGuard],
+  },
   { path: 'profile', component: ProfileUserComponent },
   { path: 'sign', component: SignInComponent },
   { path: 'accountCreate', component: AccountCreateComponent },
