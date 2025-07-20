02e73a2c8b7b13f0edc4f1dfa1327011b1e8b3b3
jodu07 <jhonnat.dussan@gmail.com>
2025-07-20 20:26:19 +0200

    Account create button updated

--- src/app/entry/account-create/account-create.component.ts
+++ src/app/entry/account-create/account-create.component.ts
@@ -6,6 +6,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
@@ -22,6 +23,7 @@ import { Customer } from 'src/models/customer';
     MatLabel,
     ReactiveFormsModule,
     MatInput,
+    MatButtonModule,
   ],
   templateUrl: './account-create.component.html',
   styleUrl: './account-create.component.scss',
